757854631db3deca2e4ae410ee409fe7fe8a8156
wonezi <139627193+wonezi@users.noreply.github.com>
2022-01-12 20:25:51 +0000

    zsh

--- src/main/scala/nlp/spark/annotate/Annotation.scala
+++ src/main/scala/nlp/spark/annotate/Annotation.scala
@@ -1,2 +1,9 @@
 
 package nlp.spark.annotate
+
+/**
+  * This class represents an annotation in text
+  * @param aType the type of the annotation (e.g. token, sentence, etc.)
+  * @param begin the index of the first character of the annotation
+  * @param end the index after the last charactor of the annotation
+  * @param metadata the metadata for the annotation (e.g. lemma, sentiment, etx.)
