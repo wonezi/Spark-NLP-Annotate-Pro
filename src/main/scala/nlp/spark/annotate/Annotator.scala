8658c25927198b459bb393b0e81d8ce7c0f8d3b9
wonezi <139627193+wonezi@users.noreply.github.com>
2022-03-26 17:54:32 +0000

    Description.

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -3,4 +3,4 @@ package nlp.spark.annotate
 import scala.collection.immutable.TreeSet
 
 /**
-  * This trait represents an function that produces annotations fr
+  * This trait represents an function that produces annotations from a giv
