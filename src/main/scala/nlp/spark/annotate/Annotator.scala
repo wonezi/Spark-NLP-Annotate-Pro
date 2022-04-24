51cb87be1aa46057208e3b31e0da3919eb59209f
wonezi <139627193+wonezi@users.noreply.github.com>
2022-04-24 20:18:32 +0000

    dm io: fix a race condition in the wake up code for sync_io

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -7,4 +7,4 @@ import scala.collection.immutable.TreeSet
   */
 trait Annotator {
   /**
-    * This takes a text, optional metadata, and optional ann
+    * This takes a text, optional metadata, and optional annota
