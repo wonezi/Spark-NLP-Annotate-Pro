cdcf1a0e292f09789cb8070eb3b2833b848752ec
wonezi <139627193+wonezi@users.noreply.github.com>
2022-05-03 13:54:44 +0000

    testing changes

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -7,4 +7,4 @@ import scala.collection.immutable.TreeSet
   */
 trait Annotator {
   /**
-    * This takes a text, optional metadata, and optional annotations and retur
+    * This takes a text, optional metadata, and optional annotations and returns
