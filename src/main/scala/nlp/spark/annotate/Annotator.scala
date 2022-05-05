bc6d0379b58fd2ce0a7de08cc8a6e12ce9765ff4
wonezi <139627193+wonezi@users.noreply.github.com>
2022-05-05 14:26:44 +0000

    Doxygen documentation

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -7,4 +7,4 @@ import scala.collection.immutable.TreeSet
   */
 trait Annotator {
   /**
-    * This takes a text, optional metadata, and optional annotations and returns a 
+    * This takes a text, optional metadata, and optional annotations and returns a sequ
