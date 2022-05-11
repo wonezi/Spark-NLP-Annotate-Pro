a722e106f9ddc5d77072484adce1eacc625617a6
wonezi <139627193+wonezi@users.noreply.github.com>
2022-05-11 06:49:06 +0000

    Updated Portuguese translation and converted to New Spelling (Novo AO)

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -7,4 +7,4 @@ import scala.collection.immutable.TreeSet
   */
 trait Annotator {
   /**
-    * This takes a text, optional metadata, and optional annotations and returns a sequ
+    * This takes a text, optional metadata, and optional annotations and returns a sequence o
