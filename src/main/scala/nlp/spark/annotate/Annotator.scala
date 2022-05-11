941412f749abfc05c98a91197619f9809b3e6c4f
wonezi <139627193+wonezi@users.noreply.github.com>
2022-05-11 13:09:19 +0000

    tooltip fix

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -7,4 +7,4 @@ import scala.collection.immutable.TreeSet
   */
 trait Annotator {
   /**
-    * This takes a text, optional metadata, and optional annotations and returns a sequence o
+    * This takes a text, optional metadata, and optional annotations and returns a sequence of new
