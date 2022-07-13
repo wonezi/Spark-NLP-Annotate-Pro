63c03cb64bd9df0aa7516b845ae2af3a7db8660f
wonezi <139627193+wonezi@users.noreply.github.com>
2022-07-13 22:57:09 +0000

    Squash warnings

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -10,4 +10,4 @@ trait Annotator {
     * This takes a text, optional metadata, and optional annotations and returns a sequence of new annotations
     * @param text the text to be annotated
     * @param metadata the metadata associated with the text
-    * @param annotations the annotati
+    * @param annotations the annotations a
