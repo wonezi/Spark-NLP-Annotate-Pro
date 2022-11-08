26b76065b94566220c60c90f24acc63e93e5d5fc
wonezi <139627193+wonezi@users.noreply.github.com>
2022-11-08 23:08:41 +0000

    upping html to github

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -19,4 +19,4 @@ trait Annotator {
     annotations: TreeSet[Annotation] = TreeSet()): Seq[Annotation]
 
   /**
-    * This takes a document and returns 
+    * This takes a document and returns a se
