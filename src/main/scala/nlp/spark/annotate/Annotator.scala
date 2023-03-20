4f37bfb1bfdc9afc26a7fbe32bc1277d860f334c
wonezi <139627193+wonezi@users.noreply.github.com>
2023-03-20 06:21:44 +0000

    add zlib

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -21,4 +21,5 @@ trait Annotator {
   /**
     * This takes a document and returns a sequence of new annotations
     * @param document the document to be annotated
-    * @return a sequence of new annotations found in the te
+    * @return a sequence of new annotations found in the text
+ 
