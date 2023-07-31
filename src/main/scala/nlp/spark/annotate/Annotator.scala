1afa1e3044a9be8f49c62ef64c70e793b56735ac
wonezi <139627193+wonezi@users.noreply.github.com>
2023-07-31 12:06:47 +0000

    Ignore dist directory

--- src/main/scala/nlp/spark/annotate/Annotator.scala
+++ src/main/scala/nlp/spark/annotate/Annotator.scala
@@ -26,4 +26,4 @@ trait Annotator {
   def annotate(document: Document): Seq[Annotation] = annotate(document.text, document.metadata, document.annotations)
 
   /**
-    * This takes a document and returns a new document with new annotations (
+    * This takes a document and returns a new document with new annotations (if 
