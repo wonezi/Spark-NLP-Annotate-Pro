9c43d60d60459bad1c939ecbe8c81685c7d3b48f
wonezi <139627193+wonezi@users.noreply.github.com>
2024-03-15 11:20:24 +0000

    New translations en.json (Hindi)

--- src/main/scala/nlp/spark/annotate/Document.scala
+++ src/main/scala/nlp/spark/annotate/Document.scala
@@ -4,3 +4,9 @@ package nlp.spark.annotate
 import scala.collection.immutable.TreeSet
 
 /**
+  *
+  * @param docName the name of the document
+  * @param text the contents of the document
+  * @param metadata the metadata associated with the document
+  * @param annotations the annotations found in the document
+  */
