41fdd0fcea189fb4a353100302b083f7974e52d2
wonezi <139627193+wonezi@users.noreply.github.com>
2024-04-13 08:22:43 +0000

    style nit

--- src/test/scala/nlp/spark/annotate/AnnotationTest.scala
+++ src/test/scala/nlp/spark/annotate/AnnotationTest.scala
@@ -4,3 +4,9 @@ package nlp.spark.annotate
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+
+import scala.collection.immutable.TreeSet
+
+@RunWith(classOf[JUnitRunner])
+class AnnotationTest extends FunSuite {
+  test("ordering") {
